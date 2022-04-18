9e6525ab7549e8c76c6a4466c403de6616168138
Hugo Pascoal <hugo_pascoal_@hotmail.com>
2022-04-18 10:46:40 -0300

    trait to test auth

--- tests/Feature/Api/Auth/AuthTest.php
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -5,10 +5,12 @@ namespace Tests\Feature\Api\Auth;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\UtilsTrait;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use UtilsTrait;
     /**
      * A basic feature test example.
      *
@@ -38,8 +40,7 @@ class AuthTest extends TestCase
     }
     public function test_logout()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('teste')->plainTextToken;
+        $token = $this->createTokenUser();
 
         $response = $this->postJson('/logout', [], [
             'Authorization' => "Bearer {$token}",
@@ -57,8 +58,8 @@ class AuthTest extends TestCase
     public function test_get_met()
     {
 
-        $user = User::factory()->create();
-        $token = $user->createToken('teste')->plainTextToken;
+
+        $token = $this->createTokenUser();
         $response = $this->getJson('/me', [
             'Authorization' => "Bearer {$token}",
         ]);
